b390e12a4828fe89468159b899ca35c9de07485f
seif belhaj <seifbhwc1@gmail.com>
2023-03-30 09:36:24 +0100

    fixed node err

--- client/src/fib.js
+++ client/src/fib.js
@@ -35,7 +35,10 @@ class Fib extends Component {
   };
 
   renderSeenIndexes() {
-    return this.state.seenIndexes.map(({ number }) => number).join(', ');
+    return this.state.seenIndexes.map(({ number }) => {
+      console.log(number)
+      return number
+    }).join(', ');
   }
 
   renderValues() {
